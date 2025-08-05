e2bb3834377558b7ba0e61336da18c77a043a867
BrunoAllan <bruno.allan1412@gmail.com>
2025-08-05 16:18:16 -0300

    Update Program.cs
    Adicionado o sistema de divisão

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,6 +4,11 @@ using System;
 {
     static void Main()
     {
+        Console.WriteLine("Digite o primeiro número");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nDigite o segundo número");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Resultado da Divisão: {a / b}");
     }
    
  }
