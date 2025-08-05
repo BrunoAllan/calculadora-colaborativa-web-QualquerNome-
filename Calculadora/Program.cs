601f773b85044bd2af324f3073cdcf9bce98be2d
Felipe-Marcos-De-Souza <Felipinhomarcos582@gmail.com>
2025-08-05 16:38:12 -0300

    Update Program.cs
    Adicionada a parte de multiplicação

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-﻿class Program
+class Program
 {
     static void Main()
     {
+        
     }
+
+    static void Multiplicacao ()
+        {
+            // Pedindo os números para o usuário:
+            Console.WriteLine("Escreva o primeiro número:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva o Segundo número:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Seu resultado é: {num1 * num2}");
+        }
    
  }
