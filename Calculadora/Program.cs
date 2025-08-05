d0e8798f5976b9660ab2f4233b3dd9b3bfa3ef9c
Igor9001 <igoroliveiramellosilva@gmail.com>
2025-08-05 16:33:12 -0300

    Update Program.cs
    A parte da subtração

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,6 +4,18 @@ using System;
 {
     static void Main()
     {
+        
     }
-   
+
+    static void subtração()
+    {
+         //Entrada dos números
+        Console.WriteLine("Digite o primeiro número");
+        int sub1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número");
+        int sub2 = int.Parse(Console.ReadLine());
+
+        //Resultado
+        Console.WriteLine($"A subtração dos números é: {sub1 - sub2}");
+    }
  }
